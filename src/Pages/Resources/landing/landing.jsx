e344491f5871752c9915009aca90f7b10d7554aa
Javed Shah <>
2026-01-03 12:34:45 -0600

    photo

--- src/Pages/Resources/landing/landing.jsx
+++ src/Pages/Resources/landing/landing.jsx
@@ -1,5 +1,5 @@
 import './landing.css'
-import Sujot from '../../../assets/Images/Sujot.jpg'
+import Faraaz from '../../../assets/Images/Faraaz.jpeg'
 import github from "../../../assets/Github.svg"
 import linkedin from "../../../assets/Linkedin.svg"
 import gmail from "../../../assets/Gmail.svg"
@@ -10,16 +10,16 @@ const Landing = () => {
     <>
       <div className="jumbotron" style={{ backgroundColor: '#F7F7F2' }}>
         <div className="container ">
-          <h1 className="display-3 d-flex justify-content-center"><img src={Sujot} className="display-pics" alt="Sujot Singh" /></h1>
+          <h1 className="display-3 d-flex justify-content-center"><img src={Faraaz} className="display-pics" alt="Faraaz Shah" /></h1>
           <div className='d-flex justify-content-center'>
-            <h1 className="display-3 d-flex justify-content-center px-2" style={{color: "#39392D"}}>Sujot</h1>
-            <h1 className="display-3 d-flex justify-content-center px-1" style={{color: "#39392D"}}>Singh</h1>
+            <h1 className="display-3 d-flex justify-content-center px-2" style={{color: "#39392D"}}>Faraaz</h1>
+            <h1 className="display-3 d-flex justify-content-center px-1" style={{color: "#39392D"}}>Shah</h1>
           </div>
           <h4 className='text-center' style={{ color: '#39392D' }}><i>"It is what we know already that often prevents us from learning."</i></h4>
 
           <div className="col-md-12 d-flex justify-content-end align-items-bottom">
               <a className='px-2 text-right' href="mailto:sfaraazio@gmail.com"><img width="20" height="20" src={gmail} className="logo" alt="mail" style={{verticalAlign: 'middle', marginTop: "5px"}}/></a>
-              <a className='px-2 text-right' href="https://www.linkedin.com/in/sujot/"><img width="20" height="20" src={linkedin} className="logo" alt="linkedin" /></a>
+              <a className='px-2 text-right' href="https://www.linkedin.com/in/faraazjshah/"><img width="20" height="20" src={linkedin} className="logo" alt="linkedin" /></a>
               <a className='px-2 text-right' href="https://github.com/Faraazio"><img width="20" height="20" src={github} className="logo" alt="github" /></a>
               <a
                 className="px-2 text-right"
@@ -108,18 +108,6 @@ const Landing = () => {
                <a href="https://github.com/Faraazio" style={{color:"#557E25"}}>Github</a><br></br>
             </p>
 
-            <div className="card my-2" style={{ backgroundColor: "#F2F3E2", border: 'none' }}>
-              <div className="card-body text-center">
-                <img 
-                  src={`https://ghchart.rshah.org/Faraazio?${new Date().getTime()}`} 
-                  alt="GitHub chart of Sujot" 
-                  className="img-fluid" 
-                  style={{ maxWidth: '100%', height: 'auto' }} 
-                />
-                <h6 className="card-title" style={{ color: "#48483A" }}><i>Current Github Activity</i></h6>
-              </div>
-
-            </div>
 
           </div>
         </div>
